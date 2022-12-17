c2774fa62644417aff69c5e734d9a288d1ef5eb8
goiisan-01 <training@goiisan.com>
2022-12-17 10:34:29 -0600

    deploy terraform

--- iam.tf
+++ iam.tf
@@ -2,13 +2,71 @@ resource "aws_iam_group" "developers" {
   name = "developers"
 }
 
+resource "aws_iam_group_policy" "developer_policy" {
+  name  = "developer_policy"
+  group = aws_iam_group.developers.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
 resource "aws_iam_group" "devops" {
   name = "devops"
+}
+
+resource "aws_iam_group_policy" "devops_policy" {
+  name  = "devops_policy"
+  group = aws_iam_group.devops.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:*",
+          "vpc:*",
+          "ec2:*",
+          "iam:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
 
 resource "aws_iam_group" "admin" {
   name = "admin"
 }
+
+resource "aws_iam_group_policy" "admin_policy" {
+  name  = "admin_policy"
+  group = aws_iam_group.admin.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:*",
+          "iam:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+} 
 
 resource "aws_iam_user" "main" {
   name = "Test-User"
